cd01cfab3900c16f6632872f71386db07cd4a8ee
2laJ2 <tuulajakobsson@hotmail.com>
2023-11-29 11:28:22 +0200

    login, logout buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,45 @@
 import './PhaserGame'
 import './App.css'
+import {useState} from 'react';
 
-const App = () => <div>
-  <div id="phaser-container"></div>
-  <div id="ui-container">
-    <h1>Room game</h1>
-  </div>
-</div>
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  function login() {
+    setAuthenticated(true);
+  }
+
+  function logout() {
+    setAuthenticated(false);
+  }
+
+  if (authenticated) {
+    return (
+      <div>
+        <div id="phaser-container"></div>
+        <div id="ui-container">
+          <h1>Room game</h1>
+          <button onClick={logout}>
+            Logout
+          </button>  
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <Login login={login}>
+    </Login>
+  )
+}
+
+function Login({login}) {
+  return (
+    <div>
+      <input>
+      </input>
+      <button onClick={login}>Login</button>
+    </div>    
+  )
+}
 
 export default App
